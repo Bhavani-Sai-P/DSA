2133ce22d0551611a0a095446b335bfa934863fc
Bhavani Sai P <pbsn99@gmail.com>
2026-04-30 16:02:12 -0500

    Time: 8 ms (53.63%) | Memory: 43.8 MB (86.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        //frequency map for s1 (char, count).
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-            char c = s1.charAt(i);
-            map.put(c, map.getOrDefault(c, 0)+1);
-        }
-
-        // move sliding window over s2
-        Map<Character, Integer> map2 = new HashMap<>();
-        int i=0;
-        for(int j=0;j<s2.length();j++){
-            char ch = s2.charAt(j);
-            map2.put(ch, map2.getOrDefault(ch, 0)+1);
-            if(j-i+1 > s1.length()){
-                char left = s2.charAt(i);
-                map2.put(left, map2.get(left) - 1);
-                if(map2.get(left) == 0) map2.remove(left);
-                i++;
+        if (s1.length() > s2.length()) return false;
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+        for (int i = 0; i < s1.length(); i++) count1[s1.charAt(i) - 'a']++;
+        int left = 0;
+        for (int right = 0; right < s2.length(); right++) {
+            count2[s2.charAt(right) - 'a']++;
+            if (right - left + 1 > s1.length()) {
+                count2[s2.charAt(left) - 'a']--;
+                left++;
             }
-            if(j-i+1 == s1.length() && map.equals(map2)) return true;
+            if (right - left + 1 == s1.length()) {
+                if (Arrays.equals(count1, count2)) return true;
+            }
         }
         return false;
     }
